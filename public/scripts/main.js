a0e3489a38f7fda7ada4a26a6c6c70f5148fe131
Lica <licangel@msn.com>
2019-02-06 13:26:23 -0700

    Finish the home page with its respective nav styles

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,7 @@
 const btns = document.getElementsByClassName('btn');
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav');
+const modal = document.getElementById('modal');
 
 for (const btn of btns) {
 	btn.addEventListener('mouseenter', function() {
@@ -9,3 +12,18 @@ for (const btn of btns) {
 		this.classList.remove('btn-hover');
 	});
 }
+
+burger.addEventListener('click', function() {
+	// Display Modal
+	modal.classList.toggle('modal-show');
+
+	// If modal is on, show black X icon; otherwise show turquoise burger icon
+	// There's a bit of a potential problem here in that our nav's default behavior inspects the presence of .modal-show as the basis for displaying dark-nav or turquoise nav; later we'll use dark nav for our lighter background pages. We'll need to change the dark-nav behavior when we get there...
+	if (modal.classList.contains('modal-show')) {
+		this.classList.add('close');
+		nav.classList.add('dark-nav');
+	} else {
+		this.classList.remove('close');
+		nav.classList.remove('dark-nav');
+	}
+});
